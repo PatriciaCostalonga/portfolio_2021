52a8b21159a60ffe257f6a88c9bc03c6266684fc
PatriciaWhitezip <patcostalonga@gmail.com>
2021-07-25 21:07:32 +0100

    Home page style

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,14 @@
 import React from "react";
+import image from "../Assets/images/sky.jpg"
 
 export default function Home(){
-    return <h1>Home Page</h1>
+    return (
+        <main>
+            <img src={image} alt="blue skies with pink clouds"
+            className="absolute object-cover w-full h-full"/>
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-8xl text-white font-bold leading-none lg:leading-snug home-name">Hello, I'm Patricia Costalonga</h1>
+            </section>
+        </main>
+    )
 }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,25 +1,61 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
 
 export default function NavBar(){
     return (
-        <header className="bg-gray-600">
+        <header className="bg-indigo-800">
             <div className="container mx-auto flex justify-between">
-                <nav>
-                    <NavLink to="/" exact>
+                <nav className="flex">
+                    <NavLink to="/" exact 
+                    activeClassName="text-rose-300" 
+                    className="inflex-flex 
+                    items-center py-6 px-3 mr-4 
+                    text-rose-100 
+                    hover:text-blue-200
+                    text-4xl font-bold tracking-widest">
                         Patricia Costalonga
                     </NavLink>
-                    <NavLink to="/post">
+                    <NavLink to="/post"  
+                    activeClassName="text-rose-300 bg-indigo-600"
+                    className="inflex-flex 
+                    items-center py-3 px-3 my-6 
+                    text-rose-100 
+                    rounded
+                    hover:text-blue-200
+                    text-2xl font-bold">
                         Blog Posts
                     </NavLink>
-                    <NavLink to="/project">
+                    <NavLink to="/project"  
+                    activeClassName="text-rose-300 bg-indigo-600"
+                    className="inflex-flex 
+                    items-center py-3 px-3 my-6 
+                    text-rose-100 
+                    rounded
+                    hover:text-blue-200
+                    text-2xl font-bold">
                         Projects
                     </NavLink>
-                    <NavLink to="/about">
+                    <NavLink to="/about"  
+                    activeClassName="text-rose-300 bg-indigo-600" 
+                    className="inflex-flex 
+                    items-center py-3 px-3 my-6 
+                    text-rose-100 
+                    rounded
+                    hover:text-blue-200
+                    text-2xl font-bold">
                         About me
                     </NavLink>
                 </nav>
+                <div className="inline-flex py-3 px-3  my-6">
+                    <SocialIcon url="https://www.linkedin.com/in/patricia-costalonga/" 
+                    className="mr-4" 
+                    target="_blank" fgColor="#fff" style={{ height:35, width:35 }}/>
+                    <SocialIcon url="https://github.com/PatriciaCostalonga" 
+                    className="mr-4" 
+                    target="_blank" fgColor="#fff" style={{ height:35, width:35 }}/>
+                </div>
             </div>
         </header>
     )
-}
+};
